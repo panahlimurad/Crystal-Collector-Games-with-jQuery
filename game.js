86ce9a34b5ffd0650856016071d40cd47286a8fa
panahlimurad <panahli.murad@gmail.com>
2023-02-07 00:06:16 +0400

    create new game

---
+++ game.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  let targetScore = $("#target_score");
+  let yourScore = $("#your_score");
+  let winCount = $("#win_count");
+  let blueCrystal = $("#blue_crystal");
+  let greenCrystal = $("#green_crystal");
+  let redCrystal = $("#red_crystal");
+  let yellowCrystal = $("#yellow_crystal");
+  let lossCount = $("#loss_count");
+});
